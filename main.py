211a05cd3365bd482f14fdecf2e57a774ecf0053
artkis74 <kiselev_artem1991@mail.ru>
2022-08-07 23:52:53 +0500

    Complited task #1

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
+import json
+import urllib.request
 from VK import VK
 from pprint import pprint
+from YandexDisk import YandexDisk
 from datetime import datetime
 
 if __name__ == '__main__':
-  with open('vk_data.txt', 'r') as file:
-    token = file.readline().strip()
-    id = file.readline().strip()
-  access_token = token
-  user_id = id
-  vk = VK(access_token, user_id)
-  answer = vk.get_photos()
-  # pprint(answer['response']['items'])
-  for data in answer['response']['items']:
-    for key, value in data.items():
-      # print(key)
-      if key == 'likes':
-        likes = value['count']
-        # print(likes)
-      elif key == 'date':
-        data = datetime.utcfromtimestamp(value).strftime('%Y-%m-%d %H:%M:%S')
-        # print(data)
-      elif key == 'sizes':
-        for photo in value:
-          if photo['type'] == "z":
-            link = photo['url']
-            # print(link)
+    my_disk = YandexDisk()
+    folder = "Final_work#1"
+    print(my_disk.create_new_folder(folder))
+    with open('vk_data.txt', 'r') as file:
+        token = file.readline().strip()
+        id_vk = file.readline().strip()
+    access_token = token
+    user_id = id_vk
+    vk = VK(access_token, user_id)
+    answer = vk.get_photos()
+    images = []
+    for data in answer['response']['items']:
+        data_image = {}
+        link = ' '.join([url['url'] for url in data['sizes'] if url['type'] == 'z'])
+        date = data['date']
+        # date = datetime.utcfromtimestamp(data['date']).strftime('%Y-%m-%d %H:%M:%S') При таком варианте выдает
+        # ошибку  'Specified path "08:01.jpg" has incorrect format', поэтому решил оставить Unix-время
+        image_name = str(data['likes']['count'])
+        if image_name + '.jpg' in ([image["file_name"] for image in images]):
+            data_image["file_name"] = image_name + " " + str(date) + '.jpg'
+        else:
+            data_image["file_name"] = image_name + '.jpg'
+        data_image["size"] = ' '.join([url['type'] for url in data['sizes'] if url['url'] == link])
+        images.append(data_image)
+        with open("images.json", 'w') as doc:
+            json.dump(images, doc)
+        urllib.request.urlretrieve(link, "photo.jpg")
+        path_to_file = folder + '/' + data_image["file_name"]
+        file = "photo.jpg"
+        pprint(my_disk.upload_file(path_to_file, file))
