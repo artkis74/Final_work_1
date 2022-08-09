a1e38395dafcbf6de2def3850f3f7c523a9c9ea7
artkis74 <kiselev_artem1991@mail.ru>
2022-08-09 21:29:02 +0500

    added edits

--- VK.py
+++ VK.py
@@ -1,23 +1,24 @@
 import requests
 
+with open('vk_data.txt', 'r') as file:
+    token = file.readline().strip()
+    id_vk = file.readline().strip()
 class VK:
 
-   def __init__(self, access_token, user_id, version='5.131'):
+   def __init__(self, access_token=token, user_id=id_vk, version='5.131'):
        self.token = access_token
        self.id = user_id
        self.version = version
        self.params = {'access_token': self.token, 'v': self.version}
 
-   def users_info(self):
-       url = 'https://api.vk.com/method/users.get'
-       params = {'user_ids': self.id}
-       response = requests.get(url, params={**self.params, **params})
-       # print(response.status_code)
-       return response.json()
-
-   def get_photos(self):
+   def get_photos_prof(self):
        url = 'https://api.vk.com/method/photos.get'
        params = {'user_ids': self.id, 'album_id': 'profile', 'extended': '1'}
        response = requests.get(url, params={**self.params, **params})
-       # print(response.status_code)
        return response.json()
+
+   def get_photos_wall(self):
+       url = 'https://api.vk.com/method/photos.get'
+       params = {'user_ids': self.id, 'album_id': 'wall', 'extended': '1'}
+       response = requests.get(url, params={**self.params, **params})
+       return  print(response.json())
